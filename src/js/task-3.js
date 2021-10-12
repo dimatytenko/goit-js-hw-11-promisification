6fb78b7e34b62b8ec712919b41adcc09bf1229c3
Dima Tytenko <amgpetronass@gmail.com>
2021-10-12 23:24:16 +0300

    fixed

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -4,20 +4,20 @@ const randomIntegerFromInterval = (min, max) => {
 
 const makeTransaction = transaction => {
   return new Promise((resolve, reject) => {
-    const delay = randomIntegerFromInterval(200, 500);
+    let delay = randomIntegerFromInterval(200, 1000);
 
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       }
       reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
